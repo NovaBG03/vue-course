0459eb87cf66bcd129bde18c8bc342ec1b28aca5
Nikita Koynov <nikicha03@gmail.com>
2021-02-09 19:35:23 +0200

    getting-started

---
+++ 1-getting-started/starting-project/app.js
@@ -0,0 +1,27 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+    },
+  },
+}).mount("#app");
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//     const listItemElement = document.createElement('li');
+//     listItemElement.textContent = enteredValue;
+//     listEl.appendChild(listItemElement);
+//     inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
